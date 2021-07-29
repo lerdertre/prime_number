f6c60289597230ad930bab890da77b5234165029
lerdertre <62736171+lerdertre@users.noreply.github.com>
2021-07-29 11:09:45 +0800

    Update calculate_prime.py

--- calculate_prime.py
+++ calculate_prime.py
@@ -1,7 +1,7 @@
 n = int(input("输入一个正整数n（n>=2）："))
 for i in range(2,n):
     if n%i==0:
-        print(n,"不是素数")
+        print("{}不是素数".format(n))
         break
 else:
-    print(n,"是素数")
+    print("是素数".format(n))
